8a0299e110a968db290408bd4db24572de8572f9
Jagannadh <asljagannadh.avidi@gmail.com>
2024-06-21 21:51:18 +0530

    Added GeniricRubicsCubeSolver Model

---
+++ GeniricRubicsCubeSolver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by len on 21-06-2024.
+//
+
+#include "GeniricRubicsCubeSolver.h"
---
+++ GeniricRubicsCubeSolver.h
@@ -0,0 +1,16 @@
+//
+// Created by len on 21-06-2024.
+//
+
+#ifndef GENIRICRUBICSCUBESOLVER_H
+#define GENIRICRUBICSCUBESOLVER_H
+
+
+
+class GeniricRubicsCubeSolver {
+
+};
+
+
+
+#endif //GENIRICRUBICSCUBESOLVER_H
